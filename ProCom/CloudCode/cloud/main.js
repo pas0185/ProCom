a31aa4b35ea0278522c438a715d1428a76756a78
Patrick Sheehan <psheehan0301@gmail.com>
2015-02-25 14:30:57 -0600

    Cleaned up main.js

--- ProCom/CloudCode/cloud/main.js
+++ ProCom/CloudCode/cloud/main.js
@@ -1,15 +1,4 @@
 require('cloud/blurb.js')
 require('cloud/userConvo.js')
 require('cloud/userGroups.js')
-
-Parse.Cloud.define("testRelation", function(request, response) {
-
-	var Convo = Parse.Object.extend("Convo");
-
-	// Get the User by its objectId
-	var qUser = new Parse.Query("User");
-	qUser.equalTo("objectId", request.params.objectId);
-
-	qUser.first({
-		success: function(user) {
-});
+require('cloud/convo.js')
