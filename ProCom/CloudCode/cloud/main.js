84cfb09a3a72cf42aa767c963cb72dca95d0796d
Patrick Sheehan <psheehan0301@gmail.com>
2015-02-25 11:16:59 -0600

    Cloud code: userConvos function gets convoIds
    Provide User’s objectId return list of convo Ids

--- ProCom/CloudCode/cloud/main.js
+++ ProCom/CloudCode/cloud/main.js
@@ -4,3 +4,19 @@
 Parse.Cloud.define("hello", function(request, response) {
 	response.success("Hello world!");
 });
+
+Parse.Cloud.define("userConvos", function(request, response) {
+	var query = new Parse.Query("User");
+	
+	query.equalTo("objectId", request.params.objectId);
+
+	query.first({
+		success: function(user) {
+			convoIds = user.get("convoIds");
+			response.success(convoIds);
+		},
+		error: function() {
+			response.error("No convos found for this user");
+		}
+	});
+});
