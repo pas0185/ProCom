5301d8685f78123588d11b5b1dd39a7c8ce55d83
Patrick Sheehan <psheehan0301@gmail.com>
2015-03-18 11:51:08 -0500

    Build succeeded after merge

--- ProCom/ProCom/ProCom-Bridging-Header.h
+++ ProCom/ProCom/ProCom-Bridging-Header.h
@@ -1,6 +1,5 @@
 #import <Parse/Parse.h>
 #import <FacebookSDK/FacebookSDK.h>
-#import "AFNetworking/AfNetworking.h"
 #import <Parse/PFObject+Subclass.h>
 #import "JSQMessagesViewController/JSQMessages.h"
 #import "JSQMessages.h"
