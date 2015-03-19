ce50296b88387d44943c1ed39c986f4be1a1455b
Patrick Sheehan <psheehan0301@gmail.com>
2015-03-19 13:49:28 -0500

    Push notification initial permissions

--- ProCom/ProCom/AppDelegate.swift
+++ ProCom/ProCom/AppDelegate.swift
@@ -39,7 +39,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        var rootGroupView = GroupTableViewController(group: nil)
 //        navController.pushViewController(rootGroupView, animated: true)        
         
+        // Push notifications
+        self.setupPushNotifications(application)
+        
         return true
+    }
+    
+    // MARK: - Push Notifications
+    func setupPushNotifications(application: UIApplication) {
+        
+        // Register for Push Notitications, if running iOS 8
+        if application.respondsToSelector("registerUserNotificationSettings:") {
+            
+            let types:UIUserNotificationType = (.Alert | .Badge | .Sound)
+            let settings:UIUserNotificationSettings = UIUserNotificationSettings(forTypes: types, categories: nil)
+            
+            application.registerUserNotificationSettings(settings)
+            application.registerForRemoteNotifications()
+            
+        } else {
+            // Register for Push Notifications before iOS 8
+            application.registerForRemoteNotificationTypes(.Alert | .Badge | .Sound)
+        }
+    }
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        println("didRegisterForRemoteNotificationsWithDeviceToken")
+        
+        let currentInstallation = PFInstallation.currentInstallation()
+        
+        currentInstallation.setDeviceTokenFromData(deviceToken)
+        currentInstallation.saveInBackgroundWithBlock { (succeeded, e) -> Void in
+            //code
+        }
+    }
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        println("failed to register for remote notifications:  (error)")
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        println("didReceiveRemoteNotification")
+        PFPush.handlePush(userInfo)
     }
     
     func signInUser(username: String, password: String, synchronous: Bool) {
